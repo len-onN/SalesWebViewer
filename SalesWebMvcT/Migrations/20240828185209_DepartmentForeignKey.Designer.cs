280a0320f0f162274443dbbd164d08887b6c6142
Lenon Fernandes Philippi <lenonphilippi@gmail.com>
2024-08-28 16:00:03 -0300

    DepartmentId ForeignKey

--- SalesWebMvcT/Migrations/20240828185209_DepartmentForeignKey.Designer.cs
+++ SalesWebMvcT/Migrations/20240828185209_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMvcT.Data;
 
 namespace SalesWebMvcT.Migrations
 {
     [DbContext(typeof(SalesWebMvcTContext))]
-    partial class SalesWebMvcTContextModelSnapshot : ModelSnapshot
+    [Migration("20240828185209_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
