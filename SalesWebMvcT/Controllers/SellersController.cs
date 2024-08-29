465645fbff2be46dd939c7715579799a290badf9
Lenon Fernandes Philippi <lenonphilippi@gmail.com>
2024-08-29 14:35:04 -0300

    Sellers Details

--- SalesWebMvcT/Controllers/SellersController.cs
+++ SalesWebMvcT/Controllers/SellersController.cs
@@ -32,12 +32,51 @@ namespace SalesWebMvcT.Controllers
             var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
         }
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var seller =_sellerService.FindById(id.Value);
+            if (seller == null)
+            {
+                return NotFound();
+            }
+            return View(seller);
         }
     }
 }
--- SalesWebMvcT/Services/SellerService.cs
+++ SalesWebMvcT/Services/SellerService.cs
@@ -4,23 +4,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvcT.Services
 {
     public class SellerService
     {  
         private readonly SalesWebMvcTContext _context;
+        
         public SellerService(SalesWebMvcTContext context)
         {
             _context = context;
         }
+        
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
         }
+
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
