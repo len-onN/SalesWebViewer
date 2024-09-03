5a4bb27486de3a479da41c1129bae05109d54669
Lenon Fernandes Philippi <lenonphilippi@gmail.com>
2024-09-03 14:16:09 -0300

    Adding grouping sales search by department

--- SalesWebMvcT/Services/SalesRecordService.cs
+++ SalesWebMvcT/Services/SalesRecordService.cs
@@ -33,5 +33,23 @@ namespace SalesWebMvcT.Services
                 .OrderByDescending(sale => sale.Date)
                 .ToListAsync();
         }
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(sale => sale.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(sale => sale.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(sale => sale.Seller)
+                .Include(sale => sale.Seller.Department)
+                .OrderByDescending(sale => sale.Date)
+                .GroupBy(sale => sale.Seller.Department)
+                .ToListAsync();
+        }
     }
 }
